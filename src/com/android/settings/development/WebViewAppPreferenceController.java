83441651b7ba62fa1e49ad8e62514c91a6c8e84b
danya2271 <dany2271@yandex.ru>
2025-02-10 17:51:43 +0500

    Revert "Move webview selection to system settings"
    This reverts commit b226ba4d473286d8a636fc38b4af6523261ceff4.

--- src/com/android/settings/development/WebViewAppPreferenceController.java
+++ src/com/android/settings/development/WebViewAppPreferenceController.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.settings.system;
+package com.android.settings.development;
 
 import android.content.Context;
 import android.content.pm.PackageInfo;
